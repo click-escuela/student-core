fdb39119b84bd8ac8d328d161c4b86d56108ef7c
umbert oscar <oscar.umbert@gmail.com>
2021-07-01 14:40:41 -0300

    create project

---
+++ src/test/java/click/escuela/student/core/studentcore/StudentCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package click.escuela.student.core.studentcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
