616ff68a0ce77f886292c3e7f3742dfde4b21cbc
clickescuela <83097465+clickescuela@users.noreply.github.com>
2021-08-24 21:41:10 -0300

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -5,5 +5,4 @@ sudo rm -rf /home/ec2-user/server/student-core/student-service.pid
 
 echo "eliminando archivo"
 
-sudo java -jar -Dspring.profiles.active=prod -Dspring.datasource.url=jdbc:mysql://clickescuela.ccmmeszml0xl.us-east-2.rds.amazonaws.com:3306/clickescuela -Dspring.datasource.username=root -Dspring.datasource.password=secret123 \
-   student-core-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > student-service.pid
+sudo java -jar student-core-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > student-service.pid
