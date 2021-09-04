69bd7ce37197c48e9bcf3ceb28237f9bc50ef13d
Evelin Aragon <eve_aragon@hotmail.com>
2021-09-04 17:49:25 -0300

    logs

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -5,4 +5,4 @@ sudo rm -rf /home/ec2-user/server/student-core/student-service.pid
 
 echo "eliminando archivo"
 
-sudo java -jar student-core-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > student-service.pid
+sudo java -jar -Dlogging.file.name=/home/ec2-user/server/student-core/debug.log student-core-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > student-service.pid
