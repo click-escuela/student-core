077e294ce909553f3aca247644a2c5dfe415696f
Evelin <eve_aragon@hotmail.com>
2021-10-16 18:00:23 -0300

    mejora de scripts

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -5,6 +5,7 @@ sudo rm -rf /home/ec2-user/server/student-core/student-service.pid
 sudo rm -rf /home/ec2-user/server/student-core/logs
 sudo rm -rf /home/ec2-user/server/student-core/server_stop.sh
 sudo rm -rf /home/ec2-user/server/student-core/fix_provileges.sh
+sudo rm -rf /home/ec2-user/server/student-core/server_prestart.sh
 sudo rm -rf /home/ec2-user/server/student-core/server_clear.sh
 
 
